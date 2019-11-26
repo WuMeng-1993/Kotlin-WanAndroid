81ed957be63e791ca7805a99b64b4c4f17e4c56d
WuMeng <541345568@qq.com>
2019-11-26 10:21:30 +0800

    1. Gradle实现统一依赖管理 2. 启动页实现Lottie动画效果

---
+++ app/src/main/java/com/estsh/kotlin_wanandroid/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.estsh.kotlin_wanandroid.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.estsh.kotlin_wanandroid.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- app/src/main/java/com/estsh/kotlin_wanandroid/main/SplashActivity.kt
+++ app/src/main/java/com/estsh/kotlin_wanandroid/main/SplashActivity.kt
@@ -1,12 +1,13 @@
-package com.estsh.kotlin_wanandroid
+package com.estsh.kotlin_wanandroid.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.estsh.kotlin_wanandroid.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
